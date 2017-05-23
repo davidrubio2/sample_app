dd5a928ec3d405656093d57c4db84555be72e3de
davidrubio2 <davidrubio2@gmail.com>
2017-05-23 10:22:15 -0600

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'root_url' 
+  
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
